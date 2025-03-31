a174ccc61f3e576bd6b2c7e4105d481af9c95490
maelgruand1 <maelgruand933@gmail.com>
2025-03-31 22:07:54 +0200

    Update script removing none valid characters

--- run.bat
+++ run.bat
@@ -4,12 +4,12 @@ echo Compilation des fichiers Java...
 javac -d bin src/com/p8/*.java
 
 if errorlevel 1 (
-    echo La compilation a échoué. Veuillez vérifier les erreurs.
+    echo La compilation a echoue. Veuillez verifier les erreurs.
     pause
     exit /b 1
 )
 
-echo Compilation réussie. Exécution de l'application...
+echo Compilation reussie. Execution de l'application...
 java -cp bin com.p8.App
 
 pause
